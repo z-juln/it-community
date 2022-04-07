1c1cd5f6450c93522b9e715d5b1a046ecdd4ed22
庄俊霖 <zhuangjunlin@hupu.com>
2022-04-07 10:15:02 +0800

    fix: 登陆注册调整

--- src/layouts/MainLayout/components/UserModal/index.tsx
+++ src/layouts/MainLayout/components/UserModal/index.tsx
@@ -1,7 +1,7 @@
 // TODO ui, valid
 import React, { memo, useState } from "react";
 import { useRecoilState } from "recoil";
-import { Modal, Button, Form, Input, Space } from "antd";
+import { Modal, Button, Form, Input, Space, message } from "antd";
 import { userInfoState } from "@/store";
 import styles from "./index.module.scss";
 import * as api from "@/apis";
@@ -29,15 +29,24 @@ const UserModal: React.FC<UserModalProps> = ({
 
   const [form] = Form.useForm<Fileds>();
   const [tab, setTab] = useState(Tab.LOGIN);
-  const isLogin = tab === Tab.LOGIN;
-  const tabLabel = isLogin ? "登录" : "注册";
+  const isLogining = tab === Tab.LOGIN;
+  const tabLabel = isLogining ? "登录" : "注册";
   const toggleTab = () =>
     setTab((tab) => (tab === Tab.LOGIN ? Tab.REGISTER : Tab.LOGIN));
 
-  const handleFinish = async ({ name, password }: Fileds) => {
-    const apiAction = isLogin ? api.login : api.register;
-    const userInfo = (await apiAction(name, password)).data;
-    setUserInfo(userInfo);
+  const handleFinish = ({ name, password }: Fileds) => {
+    const apiAction = isLogining ? api.login : api.register;
+    apiAction(name, password)
+      .then(({ data, code }) => {
+        if (code !== 1) {
+          throw new Error(`${tabLabel}失败`);
+        }
+        message.success(`${tabLabel}成功`);
+        setUserInfo(data);
+      })
+      .catch(() => {
+        message.error(`${tabLabel}失败`);
+      });
   };
 
   return (
